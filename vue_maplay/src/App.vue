6f41a50f7c683c6a538e8d568635a6544d0a48a4
h0shi <whdgus9655@naver.com>
2024-07-11 17:51:29 +0900

    vue ol 설치 test

--- vue_maplay/src/App.vue
+++ vue_maplay/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestPage from './components/TestPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestPage
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 }
 </style>
